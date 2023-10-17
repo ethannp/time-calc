e04588ec0552283bbcad8d82e5cf09c26e4dd005
Ethan <ethananhphan@gmail.com>
2023-10-17 13:17:44 -0400

    import and export

--- app.js
+++ app.js
@@ -266,3 +266,29 @@ window.onload = (event) => {
 document.getElementById("showMore").addEventListener("click", event => {
     loadData(-2)
 })
+
+function exp(){
+    let textarea = document.getElementById("impexp");
+    textarea.value = en(localStorage.getItem("data"));
+    textarea.focus();
+    textarea.select();
+    textarea.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(textarea.value).then(function() {
+        document.getElementById("info").innerHTML = "Copied to Clipboard!";
+    }, function (err) {
+        document.getElementById("info").innerHTML = "Please copy the text below.";
+    })
+    
+
+}
+
+function imp() {
+    if(document.getElementById("impexp").value != "" && confirm("Warning: This will overwrite all of your currently saved data. Proceed?")) {
+        localStorage.setItem("data", de(document.getElementById("impexp").value));
+        window.location.reload();
+    }
+}
+
+function en(c){var x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
+
+function de(b){var a,e={},d=b.split(""),c=f=d[0],g=[c],h=o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
